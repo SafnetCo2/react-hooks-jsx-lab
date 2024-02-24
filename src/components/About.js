78fc9b55a2a8eadf03d6f7fd025f8d2562dd86a0
nzi19001@byui.edu <nzi19001@byui.edu>
2024-02-24 17:06:51 +0300

    done assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { image } from "../data/data";
+import image from "../components/images/demo.png";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2 >About Me</h2>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit</p>
+      <div>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      </div>
+    </div>
+  );
 }
 
 export default About;
